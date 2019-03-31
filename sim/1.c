7a4c2cced19c67913320bb53a6b66a1bf15e0e74
Shashwat Singh <s10singh97@yahoo.in>
2019-03-31 13:16:05 +0530

    lab_assignments

--- sim/1.c
+++ sim/1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+#define MAX 30
 int main()
 {
-    int a[3][3], b[3][3], result[3][3], r1, c1, r2, c2, i, j, k;
+    int a[MAX][MAX], b[MAX][MAX], result[MAX][MAX], r1, c1, r2, c2, i, j, k;
 
     // printf("Enter rows and column for first matrix: ");
     // scanf("%d %d", &r1, &c1);
@@ -22,43 +22,43 @@ int main()
 
     // Storing elements of first matrix.
     printf("\nEnter elements of matrix 1:\n");
-    for(i=0; i<3; ++i)
-        for(j=0; j<3; ++j)
+    for(i=0; i<MAX; ++i)
+        for(j=0; j<MAX; ++j)
         {
             a[i][j] = 2;
         }
 
     // Storing elements of second matrix.
     printf("\nEnter elements of matrix 2:\n");
-    for(i=0; i<3; ++i)
-        for(j=0; j<3; ++j)
+    for(i=0; i<MAX; ++i)
+        for(j=0; j<MAX; ++j)
         {
             b[i][j] = 3;
         }
 
     // Initializing all elements of result matrix to 0
-    for(i=0; i<3; ++i)
-        for(j=0; j<3; ++j)
+    for(i=0; i<MAX; ++i)
+        for(j=0; j<MAX; ++j)
         {
             result[i][j] = 0;
         }
 
     // Multiplying matrices a and b and
     // storing result in result matrix
-    for(i=0; i<3; ++i)
-        for(j=0; j<3; ++j)
-            for(k=0; k<3; ++k)
+    for(i=0; i<MAX; ++i)
+        for(j=0; j<MAX; ++j)
+            for(k=0; k<MAX; ++k)
             {
                 result[i][j]+=a[i][k]*b[k][j];
             }
 
     // Displaying the result
     printf("\nOutput Matrix:\n");
-    for(i=0; i<3; ++i)
-        for(j=0; j<3; ++j)
+    for(i=0; i<MAX; ++i)
+        for(j=0; j<MAX; ++j)
         {
             printf("%d  ", result[i][j]);
-            if(j == 3-1)
+            if(j == MAX-1)
                 printf("\n\n");
         }
     return 0;
